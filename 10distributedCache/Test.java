2f2550181f8d4ecf0d4c865ec926b98215a9c97b
xulei12581 <48116516+xulei12581@users.noreply.github.com>
2021-12-04 23:30:08 +0800

    Add files via upload

---
+++ 10distributedCache/Test.java
@@ -0,0 +1,14 @@
+package io.kimmking.rpcfx.redis;
+
+
+
+public class Test {
+    public static void main(String[] args) {
+        Service service = new Service();
+        for (int i = 0; i < 50; i++) {
+            ThreadA threadA = new ThreadA(service);
+            threadA.start();
+        }
+    }
+}
+
