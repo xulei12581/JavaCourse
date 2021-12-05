e25a432c47973e671fced6aef206b22b595c4d83
xulei12581 <48116516+xulei12581@users.noreply.github.com>
2021-12-05 22:22:50 +0800

    Update Test.java

--- 10distributedCache/Test.java
+++ 10distributedCache/Test.java
@@ -1,14 +1,19 @@
 package io.kimmking.rpcfx.redis;
 
 
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 public class Test {
     public static void main(String[] args) {
-        Service service = new Service();
+        //简单的分布式锁
+        JedisUtil jedisUtil = new JedisUtil();
         for (int i = 0; i < 50; i++) {
-            ThreadA threadA = new ThreadA(service);
+            ThreadA threadA = new ThreadA(jedisUtil);
             threadA.start();
         }
+        //分布式计数器
+        JedisUtil.setDecr("kucun",0);
     }
 }
 
